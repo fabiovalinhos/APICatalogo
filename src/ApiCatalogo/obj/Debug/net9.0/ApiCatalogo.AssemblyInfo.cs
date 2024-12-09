214b7a9d1e93cd1b569879df44d5c0e0db7347c0
fabiovalinhos <fabiovalinhos@gmail.com>
2024-12-09 19:02:25 -0300

    Criado o gitignore

--- src/ApiCatalogo/obj/Debug/net9.0/ApiCatalogo.AssemblyInfo.cs
+++ src/ApiCatalogo/obj/Debug/net9.0/ApiCatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+908b7dde69c1786fa88e0428295fbfbdac22a305")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiCatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
