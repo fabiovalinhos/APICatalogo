8e0552ed8a4265ee53cc026fd20860ea1c968853
Fabio Vilalba <fabio.vilalba@topcode.com.br>
2025-01-07 14:03:41 -0300

    Criado dois tipos de validações para o Produto

--- src/ApiCatalogo/Models/Produto.cs
+++ src/ApiCatalogo/Models/Produto.cs
@@ -5,7 +5,7 @@ using System.Text.Json.Serialization;
 namespace ApiCatalogo.Models;
 
 [Table("Produtos")]
-public class Produto
+public class Produto : IValidatableObject
 {
     [Key]
     public int ProdutoId { get; set; }
@@ -13,6 +13,7 @@ public class Produto
     [Required(ErrorMessage = "O nome é obrigatório")]
     [StringLength(20, ErrorMessage = "O nome deve ter entre 5 a 20 caracteres",
     MinimumLength = 5)]
+    //[PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
 
     [Required]
@@ -36,4 +37,27 @@ public class Produto
 
     [JsonIgnore]
     public Categoria? Categoria { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new
+                    ValidationResult("A primeira letra do produto deve ser maiúscula",
+                    new[]
+                    { nameof(this.Nome)});
+            }
+
+            if (this.Estoque <= 0)
+            {
+                yield return new ValidationResult
+                    ("O estoque deve ser maior que zero",
+                    new[]
+                    { nameof(this.Estoque)});
+            }
+        }
+    }
 }
---
+++ src/ApiCatalogo/Validations/PrimeiraLetraMaiusculaAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiCatalogo.Validations
+{
+    public class PrimeiraLetraMaiusculaAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value,
+            ValidationContext validationContext)
+        {
+            if (value is null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            var primeiraLetra = value.ToString()[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                return new ValidationResult(
+                    "A primeira letra do nome do produto deverá ser maiúscula");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
