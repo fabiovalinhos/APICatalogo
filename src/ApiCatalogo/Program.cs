1e06a2bd29ac5e90a90c357b7cd4437ca6324deb
fabiovalinhos <fabiovalinhos@gmail.com>
2025-02-01 17:35:53 -0300

    Criado o repositório genérico

--- src/ApiCatalogo/Program.cs
+++ src/ApiCatalogo/Program.cs
@@ -31,6 +31,7 @@ options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
 builder.Services.AddScoped<ApiLoggingFilter>();
 builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
 builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 
 builder.Logging.AddProvider(new CustomLoggerProvider(new CustomLoggingProviderConfiguration
