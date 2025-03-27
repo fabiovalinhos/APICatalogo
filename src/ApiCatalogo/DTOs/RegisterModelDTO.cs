e38d687c3a2401c65c133e34930e6344dddd70c8
Fabio Vilalba <fabio.vilalba@topcode.com.br>
2025-03-27 14:38:57 -0300

    Criando os DTO´s para o login com JWT

---
+++ src/ApiCatalogo/DTOs/RegisterModelDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiCatalogo.DTOs
+{
+    public class RegisterModelDTO
+    {
+        [Required (ErrorMessage ="User name is required")]
+        public string?  Username { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string? Email { get; set; }
+
+        [Required (ErrorMessage= "Password is required")]
+        public string? Password { get; set; }
+    }
+}
